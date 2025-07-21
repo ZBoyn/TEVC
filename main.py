fa28c9f86f8a263ef157385b6c2fb50f18f99675
ZBoyn <279782706@qq.com>
2025-07-21 13:16:38 +0800

    fix params

--- main.py
+++ main.py
@@ -19,6 +19,13 @@ def main():
         'put_off_mutation_strength': 2
     }
     
+    # 初始化参数
+    INIT_PARAMS = {
+        'h1_count': 1,  # 使用Heuristic 1生成5个解
+        'h2_count': 1,  # 使用Heuristic 2生成5个解
+        'mutation_swaps': 30  # 随机生成30次交换的解
+    }
+    
     try:
         problem_def = load_problem_from_file(DATA_FILE_PATH)
     except FileNotFoundError:
@@ -29,7 +36,8 @@ def main():
         problem_def=problem_def,
         pop_size=POP_SIZE,
         max_generations=MAX_GENERATIONS,
-        bfo_params=BFO_PARAMS 
+        bfo_params=BFO_PARAMS,
+        init_params=INIT_PARAMS
     )
     
     final_population = algorithm.run()
